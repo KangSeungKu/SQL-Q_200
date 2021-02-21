1089fcade9dcbd851d7fab7b5fdd437329514db5
SeungKu_Kang <kangsg26@naver.com>
2021-02-21 23:56:25 +0900

    * 4일차 수정
      - 011 수정

--- PART1_입문/011.sql
+++ PART1_입문/011.sql
@@ -8,3 +8,18 @@ SELECT ename, sal
 SELECT ename, sal
   FROM emp
   WHERE (sal >= 1000 AND sal <= 3000);
+  
+  
+-- 월급이 1000에서 3000사이가 아닌 사원들을 검색
+SELECT ename, sal
+  FROM emp
+  WHERE sal NOT BETWEEN 1000 AND 3000;
+  
+SELECT ename, sal
+  FROM emp
+  WHERE (sal < 1000 OR sal > 3000);
+  
+-- 1982년도에 입사한 사원들의 이름과 입사일을 검색
+SELECT ename, hiredate
+  FROM emp
+  WHERE hiredate BETWEEN '1982/01/01' AND '1982/12/31';
