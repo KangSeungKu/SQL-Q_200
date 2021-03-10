7e91dda1df827e8aaaee52fc7dfb914e1b4d2ab3
SeungKu_Kang <kangsg26@naver.com>
2021-03-10 23:51:18 +0900

    * [8일차] - 추가분
    	- SUM OVER

---
+++ PART2_SQL_기초_다지기/050.sql
@@ -0,0 +1,6 @@
+-- 050) 데이터 분석 함수로 누적 데이터 출력하기 (SUM OVER)
+SELECT empno, ename, sal, SUM(SAL) OVER (ORDER BY empno ROWS
+                                            BETWEEN UNBOUNDED PRECEDING
+                                            AND CURRENT ROW) 누적지
+  FROM emp
+  WHERE job in ('ANALYST', 'MANAGER');
