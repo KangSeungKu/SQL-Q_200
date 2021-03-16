afde08423d759f3cd3f79c5156af02146e7be9d4
SeungKu_Kang <kangsg26@naver.com>
2021-03-16 23:56:43 +0900

    * [8일차] 수정* [8일차] 수정* [8일차] 수정* [8일차] 수정* [8일차] 수정*   [8일차] 수정* [8일차] 수정* [8일차] 수정

--- PART2_SQL_기초_다지기/050.sql
+++ PART2_SQL_기초_다지기/050.sql
@@ -4,3 +4,8 @@ SELECT empno, ename, sal, SUM(SAL) OVER (ORDER BY empno ROWS
                                             AND CURRENT ROW) 누적지
   FROM emp
   WHERE job in ('ANALYST', 'MANAGER');
+-- 괄호 안에는 값을 누적할 윈도우를 지정
+-- 윈도우의 기준은 ROWS로 함
+-- UNBOUNDED PRECEDING : 맨 첫 번쨰 행을 가리킵니다.
+-- UNBOUNDED FOLLOWING : 맨 마지막 행을 가리킵니다.
+-- CURRENT ROW : 현재 행을 가리킵니다.
