42e952e0b02da0a7a652513caa50abf66aec5088
SeungKu_Kang <kangsg26@naver.com>
2021-03-31 23:48:13 +0900

    * [10일차] - 추가분
    	- LEFT OUTER JOIN
    	- RIGHT OUTER JOIN
    	- FULL OUTER JOIN

---
+++ PART3_SQL_실력_다지기/066.sql
@@ -0,0 +1,14 @@
+-- 066) 여러 테이블의 데이터를 조인해서 출력하기 (8) (FULL OUTER JOIN)
+SELECT e.ename as 이름, e.job as 직업, e.sal as 월급, d.loc as 부서위치
+  FROM emp e FULL OUTER JOIN dept d
+  ON (e.deptno = d.deptno);
+  
+-- (=)
+
+SELECT e.ename as 이름, e.job as 직업, e.sal as 월급, d.loc as 부서위치
+  FROM emp e LEFT OUTER JOIN dept d
+  ON (e.deptno = d.deptno)
+UNION
+SELECT e.ename as 이름, e.job as 직업, e.sal as 월급, d.loc as 부서위치
+  FROM emp e RIGHT OUTER JOIN dept d
+  ON (e.deptno = d.deptno)
