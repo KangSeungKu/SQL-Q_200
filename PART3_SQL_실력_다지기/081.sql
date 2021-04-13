0d1ae630fcb31e9a2f79b56d1c8c4c28dbb6af36
SeungKu_Kang <kangsg26@naver.com>
2021-04-13 23:51:32 +0900

    * [12일차] - 진행중
    	- COMMIT
    	- ROLLBACK
    	- SAVEPOINT

---
+++ PART3_SQL_실력_다지기/081.sql
@@ -0,0 +1,6 @@
+-- 081) 데이터 저장 및 취소하기 (COMMIT, ROLLBACK)
+
+-- TCL(Transaction Control Language) : 수행한 DML작업들을 영구히 반영하는 것을 말함
+--  - COMMIT : 모든 변경 사항을 데이터베이스에 반영
+--  - ROLLBACK : 모든 변경 사항을 취소
+--  - SAVEPOINT : 특정 지점까지의 변경을 취소
